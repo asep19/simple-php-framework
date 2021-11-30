1b81f271935db1da83958be8a5d1e844d99b28a3
asep19 <aseprendi487@gmail.com>
2021-11-30 22:50:06 +0700

    good job

--- framework/core/Framework.class.php
+++ framework/core/Framework.class.php
@@ -79,4 +79,34 @@
 
   }
 
+  class Controller {
+    // Base Controller has a property called $loader, it is an instance of Loader class(introduced later)
+    protected $loader;
+
+    public function __construct() {
+      $this->loader = new Loader();
+    }
+
+    public function redirect($url, $message, $wait = 0) {
+      if($wait == 0) {
+        header("location:$url");
+      } else {
+        include CURR_VIEW_PATH . "message.html";
+      }
+      exit;
+    }
+  }
+
+  class Loader {
+    // Load library classes 
+    public function library($lib) {
+      include LIB_PATH . "$lib.class.php";
+    }
+
+    // loader helper functions. Naming conversion is xxx_helper.php
+    public function helper($helper) {
+      include HELPER_PATH . "{$helper}.class.php";
+    }
+  }
+
 ?>
